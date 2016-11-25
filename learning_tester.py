e5e58b441cce8c0d075189edc3845dacc1286347
Andras Sore <andras08@gmail.com>
2016-11-25 22:50:01 +0100

    added confusion matrix to tester script

--- learning_tester.py
+++ learning_tester.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_score
+from operator import iadd
 import numpy as np
+import numpy.matlib as matlib
 import pokerbot.database
 from tabulate import tabulate
 
@@ -24,13 +26,38 @@ y_test = np.array(y)
 #results = cross_val_score(model, X[:2000], y[:2000], cv = fold_number)
 #print("Average training accuracy: {}".format(results.mean()))
 
+
 def train_with_layers(layer_sizes):
     model = MLPClassifier(hidden_layer_sizes = layer_sizes, max_iter = 2000)
     print('Training with {}'.format(layer_sizes))
     return model.fit(X_train, y_train).score(X_test, y_test)
 
-layer_sizes = [[5, 5], [7, 5], [15, 12], [20, 17], [15, 12, 10], [30, 25]]
 
-results = [train_with_layers(s) for s in layer_sizes]
+#layer_sizes = [[5, 5], [7, 5], [15, 12], [20, 17], [15, 12, 10], [30, 25]]
+#results = [train_with_layers(s) for s in layer_sizes]
+#print(tabulate(zip(layer_sizes, results), headers=['Layer sizes', 'Score']))
 
-print(tabulate(zip(layer_sizes, results), headers=['Layer sizes', 'Score']))
+
+def get_confusion_matrix(results):
+    answer_comp = list(zip(y_test, results))
+
+    conf = matlib.zeros((3, 3))
+    for comp in answer_comp:
+        conf[comp[0] + 1,comp[1] + 1] += 1
+
+    conf = conf / len(answer_comp)
+
+    return [
+            ['Fold', conf[0,0], conf[0,1], conf[0,2]],
+            ['Call', conf[1,0], conf[1,1], conf[1,2]],
+            ['Raise', conf[2,0], conf[2,1], conf[2,2]],
+    ]
+
+
+model = MLPClassifier(hidden_layer_sizes = [20, 17], max_iter = 2000)
+results = model.fit(X_train, y_train).predict(X_test)
+
+headers = ['Act\Pred', 'Fold', 'Call', 'Raise']
+print(tabulate(get_confusion_matrix(results), headers=headers, floatfmt='.2f'))
+
+
